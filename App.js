d3765cbff49d02e7504dc91d5bad5f5190a79e8f
TedHopewell <hopewellmaome@gmail.com>
2022-10-03 16:23:19 +0200

    restaurant-app-first-commit

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View,SafeAreaView, Image } from 'react-native';
+import image1 from "./assets/dombolo1.jpg";
+
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.topView}>
-            
+          <Image source={image1} style={{width:20, height:20}}/>
       </View>
       <Text></Text>
       <StatusBar style="auto" />
@@ -16,8 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  topView:{
+    height:50,
+    width:50,
   },
 });
