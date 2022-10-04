e2e1f89e8669c4d65f24e321ec48e84bb32a1280
TedHopewell <hopewellmaome@gmail.com>
2022-10-04 16:27:35 +0200

    second-login-page-commit

--- App.js
+++ App.js
@@ -1,62 +1,25 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,SafeAreaView, Image, TextInput } from 'react-native';
-import image1 from "./assets/dombolo1.jpg";
-import image2 from "./assets/pot1.jpg"
+import * as React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginPage from './src/login';
+import SignupPage from './src/signup';
 
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.topView}>
-          <Image source={image2} style={styles.topImage}/>
-      </View>
-      <View style={styles.loginBox}>
-            <Text style={styles.loginText}>Login Here</Text>
-            <TextInput style={styles.inputBox} placeholder='Username here...'></TextInput>
-            <TextInput style={styles.inputBox} placeholder='Password here...'></TextInput>
-            <Text></Text>
-      </View>
-      
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <NavigationContainer >
+            <Stack.Navigator initialRouteName='LoginPage'>
+                  {/* <Stack.Screen name='signup' options={{title:'signup', headerShown:false}}>
+                    {(props)=> <SignupPage {...props} />}
+                  </Stack.Screen> */}
+                  <Stack.Screen name='login' options={{title:'login', headerShown:false}}>
+                    {(props)=> <LoginPage {...props} />}
+                  </Stack.Screen>
+            </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  topView:{
-    flex:1,
-  },
-  topImage:{
-    height:250,
-    width:380,
-    borderBottomLeftRadius:50,
-    borderBottomRightRadius:50,
-  },
-  loginBox:{
-    flex:2,
-    display:'flex',
-    flexDirection:'column',
-    alignItems:'center'
-  },
-  loginText:{
-    color:'black',
-    fontWeight:'bolder',
-    textAlign:'center',
-    fontSize:30,
-    paddingTop:60,
-  },
-  inputBox:{
-    height:40,
-    width:'150%',
-    border:'2px solid white',
-    marginTop:20,
-    backgroundColor:'#dededeee',
-    paddingLeft:10,
-  },
-});
+export default App;
---
+++ src/signup.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { StyleSheet, Text, View,SafeAreaView, Image, TextInput, TouchableOpacity, Linking } from 'react-native';
+import image1 from "../assets/dombolo1.jpg";
+import image2 from "../assets/pot1.jpg"
+
+
+export default function SignupPage() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topView}>
+          <Image source={image2} style={styles.topImage}/>
+      </View>
+      <View style={styles.loginBox}>
+            <Text style={styles.loginText}>Register Here</Text>
+            <TextInput style={styles.inputBox} placeholder='Username here...'></TextInput>
+            <TextInput style={styles.inputBox} placeholder='Password here...'></TextInput>
+            <TouchableOpacity href='#' style={styles.hyperLink}>Forgot Password</TouchableOpacity>
+            <TouchableOpacity style={styles.loginBtn}>
+                    <Text>Sign Up</Text>
+            </TouchableOpacity>     
+      </View>
+      
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  topView:{
+    flex:1,
+  },
+  topImage:{
+    height:200,
+    width:360,
+    borderBottomLeftRadius:50,
+    borderBottomRightRadius:50,
+  },
+  loginBox:{
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    paddingBottom:180,
+    
+  },
+  loginText:{
+    color:'black',
+    fontWeight:'bolder',
+    textAlign:'center',
+    fontSize:30,
+    paddingTop:20,
+  },
+  inputBox:{
+    height:40,
+    width:'150%',
+    border:'2px solid white',
+    marginTop:20,
+    backgroundColor:'#dededeee',
+    paddingLeft:10,
+  },
+  hyperLink:{
+    width:'140%',
+    marginLeft:15,
+    marginTop:10,
+    textAlign:'Right',
+  },
+  loginBtn:{
+    backgroundColor:'orange',
+    height:35,
+    width:80,
+    textAlign:'center',
+    paddingTop:5,
+    borderRadius:8,
+    marginTop:10,
+  },
+
+});
