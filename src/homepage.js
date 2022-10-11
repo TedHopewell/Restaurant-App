28ed8522b2bd3f74b71cac3d47c6db77f9ca5adf
TedHopewell <hopewellmaome@gmail.com>
2022-10-11 11:11:44 +0200

    homepage in progress

--- src/homepage.js
+++ src/homepage.js
@@ -3,6 +3,17 @@ import React from 'react'
 import { StyleSheet, Text, View,ScrollView,Dimensions,SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native';
 import image1 from '../assets/img.jpeg'
 import image2 from '../assets/nyama1.jpg'
+import image3 from '../assets/mabele.jpg'
+import image4 from '../assets/rice.jpg'
+import image5 from '../assets/kota1.jpg'
+import pap from '../assets/pap2.jpg'
+import chicken from '../assets/chicken.jpg'
+import chicken2 from '../assets/chicken2.jpg'
+import nama from '../assets/nama2.jpg'
+import mabele from '../assets/mabele.jpg'
+import samp from '../assets/samp.jpg'
+
+
 
 export default function Homepage() {
   return (
@@ -11,6 +22,7 @@ export default function Homepage() {
                 <Text></Text>
                 <Image source={image1} style={styles.profilePic}></Image>
         </View>
+        
         <ScrollView style={styles.midView}>
                 <View style={styles.special}>
                     <View style={styles.promoTextContainer}>
@@ -20,10 +32,87 @@ export default function Homepage() {
                         </TouchableOpacity>
                     </View>
                         <Image source={image2} style={styles.adImage}></Image>
-                        
                 </View>
+                <Text>
+                <TextInput style={styles.srchBox} placeholder='Search for food' />
+                </Text>
+                <View style={styles.menuView}>
+                    <View style={styles.menuTypes}>
+                        <TouchableOpacity>
+                            <Image source={image3}  style={styles.foodTypes}/>
+                        </TouchableOpacity>
+                        <Text>Heavy Meals</Text>
+                        <View style={styles.line}></View>
+                    </View>
+                    <View style={styles.menuTypes}>
+                        <TouchableOpacity>
+                            <Image source={image4}  style={styles.foodTypes}/>
+                        </TouchableOpacity>
+                        <Text>Light Meals</Text>
+                    </View>
+                    <View style={styles.menuTypes}>
+                        <TouchableOpacity>
+                            <Image source={image5}  style={styles.foodTypes}/>
+                        </TouchableOpacity>
+                        <Text>Kota Menu</Text>
+                    </View>
+                    
+                </View>
+                
+                <View style={styles.menuList}>
+                    <View  style={styles.firstTable}>
+                        <View style={styles.mealCards}>
+                                <TouchableOpacity><Image source={pap} style={styles.meal1}></Image></TouchableOpacity>
+                                <Text style={{textAlign:'center', paddingTop:10,fontFamily:'roboto',height:30,}}>A classic kasi flavor steak and pap</Text>
+                                <TouchableOpacity style={{backgroundColor:'orange', width:120,marginLeft:15,marginTop:20,borderRadius:20,}}>
+                                    <Text style={{textAlign:'center', fontFamily:'roboto',paddingVertical:8,fontWeight:'800'}}>R80.00</Text>
+                                </TouchableOpacity>
+                        </View>
+                        <View style={styles.mealCards}>
+                                <TouchableOpacity><Image source={chicken} style={styles.meal1}></Image></TouchableOpacity>
+                                <Text style={{textAlign:'center', paddingTop:10,fontFamily:'roboto',height:30,}}>A classing pap and hardbody</Text>
+                                <TouchableOpacity style={{backgroundColor:'orange', width:120,marginLeft:15,marginTop:20,borderRadius:20,}}>
+                                    <Text style={{textAlign:'center', fontFamily:'roboto',paddingVertical:8,fontWeight:'800'}}>R60.00</Text>
+                                </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={styles.secondTable}>
+                        <View style={styles.mealCards}>
+                                <TouchableOpacity><Image source={chicken2} style={styles.meal1}></Image></TouchableOpacity>
+                                <Text style={{textAlign:'center', paddingTop:10,fontFamily:'roboto',height:30,}}>Our very own special chicken dust</Text>
+                                <TouchableOpacity style={{backgroundColor:'orange', width:120,marginLeft:15,marginTop:20,borderRadius:20,}}>
+                                    <Text style={{textAlign:'center', fontFamily:'roboto',paddingVertical:8,fontWeight:'800'}}>R100.00</Text>
+                                </TouchableOpacity>
+                        </View>
+                        <View style={styles.mealCards}>
+                                <TouchableOpacity><Image source={nama} style={styles.meal1}></Image></TouchableOpacity>
+                                <Text style={{textAlign:'center', paddingTop:10,fontFamily:'roboto',height:30,}}>pap and lamb traditionally cooked</Text>
+                                <TouchableOpacity style={{backgroundColor:'orange', width:120,marginLeft:15,marginTop:20,borderRadius:20,}}>
+                                    <Text style={{textAlign:'center', fontFamily:'roboto',paddingVertical:8,fontWeight:'800'}}>R80.00</Text>
+                                </TouchableOpacity>
+                        </View>
+                    </View>
+                    <View style={styles.thirdTable}>
+                        <View style={styles.mealCards}>
+                                <TouchableOpacity><Image source={mabele} style={styles.meal1}></Image></TouchableOpacity>
+                                <Text style={{textAlign:'center', paddingTop:10,fontFamily:'roboto',height:30,}}>A classic Mabele and beef stew</Text>
+                                <TouchableOpacity style={{backgroundColor:'orange', width:120,marginLeft:15,marginTop:20,borderRadius:20,}}>
+                                    <Text style={{textAlign:'center', fontFamily:'roboto',paddingVertical:8,fontWeight:'800'}}>R60.00</Text>
+                                </TouchableOpacity>
+                        </View>
+                        <View style={styles.mealCards}>
+                                <TouchableOpacity><Image source={samp} style={styles.meal1}></Image></TouchableOpacity>
+                                <Text style={{textAlign:'center', paddingTop:10,fontFamily:'roboto',height:30,}}>Our special samp and stew traditionally cooked</Text>
+                                <TouchableOpacity style={{backgroundColor:'orange', width:120,marginLeft:15,marginTop:20,borderRadius:20,}}>
+                                    <Text style={{textAlign:'center', fontFamily:'roboto',paddingVertical:8,fontWeight:'800'}}>R60.00</Text>
+                                </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+
         </ScrollView>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="orange" fillOpacity="1" d="M0,64L48,101.3C96,139,192,213,288,240C384,267,480,245,576,202.7C672,160,768,96,864,85.3C960,75,1056,117,1152,149.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+        
+        {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="orange" fillOpacity="1" d="M0,64L48,101.3C96,139,192,213,288,240C384,267,480,245,576,202.7C672,160,768,96,864,85.3C960,75,1056,117,1152,149.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> */}
         
     </SafeAreaView>
   );
@@ -42,15 +131,25 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-between',
         backgroundColor:'orange',
-        height:50,
+        height:80,
         width:'100%',
     },
     profilePic:{
-        height:40,
-        width:40,
+        height:50,
+        width:50,
         borderRadius:50,
         marginTop:5,
         marginRight:10,
+        alignSelf:'center',
+    },
+    srchBox:{
+        border:'2px solid orange',
+        height:45,
+        width:deviceWidth-70,
+        marginLeft:10,
+        borderRadius:8,
+        paddingHorizontal:20,
+        fontSize:12,
     },
     special:{
         display:'flex',
@@ -88,7 +187,64 @@ const styles = StyleSheet.create({
         paddingHorizontal:5,
         paddingVertical:5,
         borderRadius:20,
+        
     },
-    
-    
+    menuView:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        width:deviceWidth-50,
+        marginTop:20,
+    },
+    foodTypes:{
+        width:50,
+        height:50,
+        borderRadius:50,
+        alignSelf:'center',
+    },
+    menuTypes:{
+        textAlign:'center',
+        width:80,
+    },
+    line:{
+        width:50,
+        height:5,
+        backgroundColor:'orange',
+        alignSelf:'center',
+        borderRadius:5,
+        marginTop:5,
+    },
+    menuList:{
+       
+        height:400,
+        width:deviceWidth-50,
+        marginTop:20,
+    },
+    firstTable:{
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        marginTop:10,
+    },
+    secondTable:{
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        marginTop:10,
+    },
+    thirdTable:{
+        flexDirection:'row',
+        justifyContent:'space-evenly',
+        marginTop:10,
+    },
+    mealCards:{
+        height:220,
+        width:150,
+        backgroundColor:'white',
+        borderRadius:20,
+    },
+    meal1:{
+        height:120,
+        width:150,
+        borderRadius:20,
+        paddingRight:80,
+    }
 })
