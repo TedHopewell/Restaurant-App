71df22a421f88fdbd8e8f5d6ed24fca467e15681
TedHopewell <hopewellmaome@gmail.com>
2022-10-11 16:11:47 +0200

    home page modified

--- src/homepage.js
+++ src/homepage.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { StyleSheet, Text, View,ScrollView,Dimensions,SafeAreaView, Image, TextInput, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import image1 from '../assets/img.jpeg'
 import image2 from '../assets/nyama1.jpg'
 import image3 from '../assets/mabele.jpg'
@@ -37,11 +38,11 @@ export default function Homepage() {
   return (
     <SafeAreaView style={styles.container}>
         <View style={styles.topView}>
-                <Text></Text>
+                <Text style={{color:'white', fontSize:30,paddingTop:18, paddingLeft:20}}>Welcome,</Text><Text style={{color:'white', fontSize:30,paddingTop:18,fontWeight:'bold',marginRight:10,}}>Hopewell</Text>
                 <Image source={image1} style={styles.profilePic}></Image>
         </View>
         
-        <ScrollView style={styles.midView}>
+        <ScrollView style={styles.midView} showsVerticalScrollIndicator={false}>
                 <View style={styles.special}>
                     <View style={styles.promoTextContainer}>
                         <Text style={styles.promoText}>Get our super combo at a very low price promo ends month end</Text>
@@ -52,7 +53,7 @@ export default function Homepage() {
                         <Image source={image2} style={styles.adImage}></Image>
                 </View>
                 <Text style={styles.searchBoxContainer}>
-                    <TouchableOpacity><FontAwesomeIcon icon={faSearch} style={{paddingLeft:5,}} /></TouchableOpacity>
+                    <TouchableOpacity><FontAwesomeIcon icon={faSearch} style={{paddingLeft:5,color:'white'}} /></TouchableOpacity>
                     <TextInput style={styles.srchBox} placeholder='Search for food' />
                 </Text>
                 <View style={styles.menuView}>
@@ -60,20 +61,20 @@ export default function Homepage() {
                         <TouchableOpacity onPress={() => setShow(!show)} >
                             <Image source={image3}  style={styles.foodTypes}/>
                         </TouchableOpacity>
-                        <Text>Heavy Meals</Text>
+                        <Text style={{color:'white'}}>Heavy Meals</Text>
                         <View style={styles.line}></View>
                     </View>
                     <View style={styles.menuTypes}>
                         <TouchableOpacity onPress={() => setShowLight(!showLight)}>
                             <Image source={image4}  style={styles.foodTypes}/>
                         </TouchableOpacity>
-                        <Text>Light Meals</Text>
+                        <Text style={{color:'white'}}>Light Meals</Text>
                     </View>
                     <View style={styles.menuTypes}>
                         <TouchableOpacity onPress={() => setShowKota(!showKota)}>
                             <Image source={image5}  style={styles.foodTypes}/>
                         </TouchableOpacity>
-                        <Text>Kota Menu</Text>
+                        <Text style={{color:'white'}}>Kota Menu</Text>
                     </View>
                     
                 </View>
@@ -86,7 +87,7 @@ export default function Homepage() {
 
         </ScrollView>
         <View style={styles.bottomView}>
-            
+            <FontAwesomeIcon icon={faBars} style={{fontSize:30,}} />
         </View>
         
         {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="orange" fillOpacity="1" d="M0,64L48,101.3C96,139,192,213,288,240C384,267,480,245,576,202.7C672,160,768,96,864,85.3C960,75,1056,117,1152,149.3C1248,181,1344,203,1392,213.3L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> */}
@@ -101,13 +102,12 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor:'white',
+      backgroundColor:'black',
     },
     topView:{
         display:'flex',
         flexDirection:'row',
-        justifyContent:'space-between',
-        backgroundColor:'orange',
+        justifyContent:'space-evenly',
         height:80,
         width:'100%',
     },
@@ -116,24 +116,27 @@ const styles = StyleSheet.create({
         width:50,
         borderRadius:50,
         marginTop:5,
-        marginRight:10,
+        marginRight:20,
         alignSelf:'center',
     },
     searchBoxContainer:{
         border:'2px solid orange',
         height:45,
         flexDirection:'row',
-       paddingTop:10,
-       borderRadius:8,
+        paddingTop:5,
+        borderRadius:8,
+        width:deviceWidth-70,
+        marginLeft:10,
+        
     },
     srchBox:{
         height:25,
-        width:deviceWidth-90,
+        width:deviceWidth-110,
         marginLeft:10,
-        marginBottom:5,
         borderRadius:8,
         paddingHorizontal:20,
         fontSize:12,
+        color:'white',
     },
     special:{
         display:'flex',
@@ -185,10 +188,12 @@ const styles = StyleSheet.create({
         height:50,
         borderRadius:50,
         alignSelf:'center',
+        
     },
     menuTypes:{
         textAlign:'center',
         width:80,
+        
     },
     line:{
         width:50,
